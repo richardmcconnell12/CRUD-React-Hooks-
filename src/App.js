74f16501eaaf432f9297c71d738152f1cac3f218
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-11-14 16:09:49 -0600

    user table implemented -- problems with flex box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
-import "./App.css";
+import UserTable from "./Tables /userTables";
 
 const App = () => {
   return (
     <div className="container">
       <h1>CRUD App With React Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add User</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View Users</h2>
+          <UserTable />
+        </div>
+      </div>
     </div>
   );
 };
