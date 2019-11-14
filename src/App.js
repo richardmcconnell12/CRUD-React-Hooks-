693506f23a7b2942ae48e991a763bc8e61dc7a9c
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-11-14 16:38:40 -0600

    implementing set state hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
-import UserTable from "./Tables /userTables";
+import React, { useState } from "react";
+import UserTable from "./Tables /UserTable";
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: "Tania", username: "floppydiskette" },
+    { id: 2, name: "Craig", username: "siliconeidolon" },
+    { id: 3, name: "Ben", username: "benisphere" }
+  ];
+
+  const [users, setUsers] = useState(usersData);
+
+  const addUser = user => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  };
+
   return (
     <div className="container">
-      <h1>CRUD App With React Hooks</h1>
+      <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add User</h2>
+          <h2>Add user</h2>
         </div>
         <div className="flex-large">
-          <h2>View Users</h2>
-          <UserTable />
+          <h2>View users</h2>
+          <UserTable users={users} />
         </div>
       </div>
     </div>
